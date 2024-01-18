4bf2645620794de4b512e1b3a5b2aea6f8ad2326
aypetrunin <a.y.petrunin@gmail.com>
2024-01-19 00:29:26 +0700

    Добавление моделей

--- api/app.py
+++ api/app.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
-# from common.settings import settings
+
+from .chat_operations import router as chat_operations_router
 
 
 def create_app():
     app = FastAPI(
         debug=True,
         docs_url='/docs',
-        title='FastAPI Mongo Example2',
+        title='FastAPI Mongo Example',
         version='0.1.0',
         description='A simple example of using FastAPI with MongoDB'
     )
+    app.include_router(chat_operations_router)
 
     return app
---
+++ api/chat_operations.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+
+# from ..models.chat_operations import ChatOperationsModel
+
+
+router = APIRouter(
+    prefix="/chat_operations",
+    tags=["chat_operations"]
+)
+
+
+@router.get("/answer")
+def get_answer(question: str):
+    return question
+
+
+@router.post("/model", description='Модель ChatGPT')
+def set_model(model: str):
+    return model
+
+
+@router.post("/temterature", description='Температура ChatGPT')
+def set_temperature(temperature: int):
+    return temperature
